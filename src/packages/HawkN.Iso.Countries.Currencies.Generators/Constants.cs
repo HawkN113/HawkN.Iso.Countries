2de1bd8e1d1ae4621383ac03f41ad230b76268a4
Nikolay Selyutin <81760946+HawkN113@users.noreply.github.com>
2026-01-10 14:57:19 +0100

    feat: Add extension HawkN.Iso.Countries.Currencies package (#7)
    * feat: Add extension HawkN.Iso.Countries.Currencies package
    
    * chore: Update formating
    
    * chore: Update ci pipeline
    
    * ci: Improve CD pipeline (Publish too Github packages)
    
    * ci: Optimize references for Nuget packages
    
    * chore: Improve code
    
    * chore: Fixed code
    
    * chore: Fix code
    
    * chore: Fix code in tests

---
+++ src/packages/HawkN.Iso.Countries.Currencies.Generators/Constants.cs
@@ -0,0 +1,26 @@
+﻿namespace HawkN.Iso.Countries.Currencies.Generators;
+
+internal static class Constants
+{
+    public const string DefaultNamespace = "HawkN.Iso.Countries.Currencies";
+    public const string DiagnosticsTitle = "Generator error";
+    public const string ErrorMark = "#ERROR:";
+    public const string GeneratorName = "HawkN.Iso.Countries.Currencies.Generators source generator";
+    public const string ErrorPrefixName = "COUNTRY_CURRENCY_";
+    public static readonly string[] ExtendedSourceData =
+    [
+        "Release: release-48",
+        "CLDR URL: https://github.com/unicode-org/cldr",
+    ];
+    public static readonly string[] SystemNamespaces = [
+        "System.Linq",
+        "System.Collections.Generic",
+        "System.Collections.Immutable"
+    ];
+    public static readonly string[] ReferencesNamespaces =
+    [
+        "HawkN.Iso.Countries",
+        "HawkN.Iso.Currencies",
+        "HawkN.Iso.Countries.Currencies.Models"
+    ];
+}
---
+++ src/packages/HawkN.Iso.Countries.Currencies/Models/CountryCurrencyInfoRow.cs
@@ -0,0 +1,8 @@
+﻿namespace HawkN.Iso.Countries.Currencies.Models;
+
+internal sealed class CountryCurrencyInfoRow
+{
+    public required string CountryCode { get; init; }
+    public required string PrimaryCurrency { get; init; }
+    public IReadOnlyList<string> SecondaryCurrencies { get; init; } = [];
+}
